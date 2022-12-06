c611b31a050cfafa5c8cf746633fce6cd93c73e7
Ardit Baloku <arditbaloku@gmail.com>
2022-12-06 10:47:39 +0100

    Do some signal processing

--- lib/advent_of_code/day_06.ex
+++ lib/advent_of_code/day_06.ex
@@ -1,7 +1,17 @@
 defmodule AdventOfCode.Day06 do
   def part1(args) do
+    args
+    |> String.split("", trim: true)
+    |> Enum.chunk_every(4, 1, :discard)
+    |> Enum.find_index(fn x -> Enum.uniq(x) |> Enum.count() == 4 end)
+    |> Kernel.+(4)
   end
 
   def part2(args) do
+    args
+    |> String.split("", trim: true)
+    |> Enum.chunk_every(14, 1, :discard)
+    |> Enum.find_index(fn x -> Enum.uniq(x) |> Enum.count() == 14 end)
+    |> Kernel.+(14)
   end
 end
